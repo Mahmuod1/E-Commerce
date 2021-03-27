15e8280026a3b3de8a0659f0c076552061883492
mohamed ali <alinemrmohamed@gmail.com>
2021-03-27 21:22:56 +0200

    admin product interface

--- IPhonak Union/IPhonakUnion/src/app/dashbored/admin-add-product/admin-add-product.component.ts
+++ IPhonak Union/IPhonakUnion/src/app/dashbored/admin-add-product/admin-add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-add-product',
@@ -7,9 +8,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminAddProductComponent implements OnInit {
 
-  constructor() { }
+  registerationForm = this.fb.group({
+
+    productName: [''],
+    price: [''],
+    category: [''],
+
+    quantity: this.fb.group({
+      quantity: [''],
+      srcColor: [''],
+      srcImage: ['']
+    }),
+    alternativeQuantity: this.fb.array([]),
+
+    type: this.fb.group({
+      model: [''],
+      type: ['']
+    }),
+    alternativeType: this.fb.array([]),
+
+    rating: [''],
+    description: [''],
+    alternativeDescription: this.fb.array([]),
+
+    images: [''],
+    alternativeImages: this.fb.array([]),
+
+  });
+
+  get alternativeQuantity() {
+    return this.registerationForm.get('alternativeQuantity') as FormArray;
+  }
+
+  get alternativeType() {
+    return this.registerationForm.get('alternativeType') as FormArray;
+  }
+
+  get alternativeDescription() {
+    return this.registerationForm.get('alternativeDescription') as FormArray;
+  }
+
+  get alternativeImages() {
+    return this.registerationForm.get('alternativeImages') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+  addNewQuantity() {
+    this.alternativeQuantity.push(this.fb.control(''))
+  }
+  removeQuantity(i: any) {
+    this.alternativeQuantity.removeAt(i);
+  }
+
+
+  addNewType() {
+    this.alternativeType.push(this.fb.control(''))
+  }
+  removeType(i: any) {
+    this.alternativeType.removeAt(i);
+  }
+
+
+  addNewDescription() {
+    this.alternativeDescription.push(this.fb.control(''))
+  }
+  removeDescription(i: any) {
+    this.alternativeDescription.removeAt(i);
+  }
+
+
+  addNewImages() {
+    this.alternativeImages.push(this.fb.control(''))
+  }
+  removeImages(i: any) {
+    this.alternativeImages.removeAt(i);
+  }
+
 
 }
